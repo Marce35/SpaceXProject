cdca99519d99a80cf29623f8b2bd97a3358e7003
Marce35 <markonedelkovski2002@gmail.com>
2025-12-01 22:04:56 +0100

    removed commented code

--- SpaceXProject.api/ExternalApiClient/ExternalApiClient.cs
+++ SpaceXProject.api/ExternalApiClient/ExternalApiClient.cs
@@ -44,55 +44,5 @@ public class ExternalApiClient : IExternalApiClient
         }
 
         return _resultFactory.Success(pagedResult);
-
-        //var query = new Dictionary<string, object>();
-
-        //if (!string.IsNullOrWhiteSpace(request.Search))
-        //{
-        //    query["name"] = new Dictionary<string, object>
-        //    {
-        //        { "$regex", request.Search },
-        //        { "$options", "i" }
-        //    };
-        //}
-
-        //if (request.Type == "upcoming") query["upcoming"] = true;
-        //else if (request.Type == "past") query["upcoming"] = false;
-
-
-        //var sort = new { flight_number = request.Sort == "asc" ? "asc" : "desc" };
-
-        //var requestBody = new SpaceXQueryRequest
-        //{
-        //    Query = query,
-        //    Options = new SpaceXQueryOptions
-        //    {
-        //        Page = request.Page,
-        //        Limit = request.Limit,
-        //        Sort = sort,
-        //        Populate = ["rocket"]
-        //    }
-        //};
-
-        //var response = await _httpClient.PostAsJsonAsync("launches/query", requestBody);
-
-
-        //if (!response.IsSuccessStatusCode)
-        //{
-        //    return _resultFactory.Failure<SpaceXPagedResponse<SpaceXLaunch>>(
-        //        new Error("SpaceX_ApiError", [$"Failed to fetch launches. Status: {response.StatusCode}"]),
-        //        ResultStatusEnum.Failure);
-        //}
-
-        //var pagedResult = await response.Content.ReadFromJsonAsync<SpaceXPagedResponse<SpaceXLaunch>>();
-
-        //if (pagedResult == null)
-        //{
-        //    return _resultFactory.Failure<SpaceXPagedResponse<SpaceXLaunch>>(
-        //        new Error("SpaceX_SerializationError", ["Received empty response from SpaceX"]),
-        //        ResultStatusEnum.Failure);
-        //}
-
-        //return _resultFactory.Success(pagedResult);
     }
 }
